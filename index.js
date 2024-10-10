46123955e67ffd39b687297e4e46e0cb3bf2cbe0
RohanAdhia <rohanadhia@gmail.com>
2024-10-11 01:26:32 +0530

    Update index.js
    Added the prevent default to avoid the Tab key and F keys from jumping the UI

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// alert("Hello!")
-
-
+// alert("Test!")
 document.addEventListener('keydown', function (event) {
-
+    event.preventDefault();
     var key_pressed = event.key === " " ? "Space" : event.key;
-
     var key = document.getElementById("key");
     var div = document.getElementById("container");
-
-
-    key.textContent = key_pressed;
-
+    key.textContent = key_pressed.toLocaleUpperCase();
     key.style.display = "block";
     div.classList.remove("test");
     div.classList.add("main");
-    console.log(key_pressed, event.location);
 });
