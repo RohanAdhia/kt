c5b416c204856003702702426ce524ca72d71481
RohanAdhia <rohanadhia@gmail.com>
2024-10-11 14:21:20 +0530

    Update index.js
    Added glowing effects when button is pressed

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-// alert("Test!")
-document.addEventListener('keydown', function (event) {
-    event.preventDefault();
-    var key_pressed = event.key === " " ? "Space" : event.key;
-    var key = document.getElementById("key");
-    var div = document.getElementById("container");
-    key.textContent = key_pressed.toLocaleUpperCase();
-    key.style.display = "block";
-    div.classList.remove("test");
-    div.classList.add("main");
-});
+    // alert("Test!")
+    document.addEventListener('keydown', function (event) {
+        event.preventDefault();
+        var key_pressed = event.key === " " ? "Space" : event.key;
+        var key = document.getElementById("key");
+        var div = document.getElementById("container");
+        key.textContent = key_pressed.toLocaleUpperCase();
+        key.style.display = "block";
+        div.classList.remove("test");
+        div.classList.add("main","glow");
+    });
+
+    document.addEventListener('keyup', function() {
+        var div = document.getElementById("container");
+        div.classList.remove('glow');
+    });
